8a36664c1d45e10959fecce5d3c54346f4cf0678
Jimmy Wärting <jimmy@warting.se>
2023-03-20 15:33:54 +0100

    minify

--- mod.js
+++ mod.js
@@ -2,20 +2,6 @@
 
 /**
  * @param {string} name The name of the module.
- * @param {string} version The version of the module.
+ * @param {string} version The version of the module you are using.
  */
-export function updateNotifier (name, version) {
-  return globalThis.fetch ? fetch('https://registry.npmjs.org/' + name)
-    .then(res => res.json())
-    .then(json => {
-      const latest = json['dist-tags'].latest.split('-')[0]
-
-      if (latest.localeCompare(version, 'en', { numeric: true }) === 1) {
-        const msg = `New version of "${name}" is available: ${version} → ${latest}`
-        console.info(
-          `\n${'='.repeat(msg.length)}\n${msg}\n${'='.repeat(msg.length)}\n\n`
-        )
-      }
-    })
-    .catch(() => { }) : void 0
-}
+export async function updateNotifier(name,version){await globalThis.fetch?.('https://registry.npmjs.org/'+name).then(e=>e.json()).then((a)=>{var i=a['dist-tags'].latest.split('-')[0],s=`New version of "${name}" is available: ${version} → `+i,n='\n',o=n+'='.repeat(s.length)+n;i.localeCompare(version,'en',{numeric:!0})>0&&console.info(o+s+o+n)}).catch(e=>e)}
